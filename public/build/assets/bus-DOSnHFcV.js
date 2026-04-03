beb0f52147ba13ad31436fc7c74a759e65595425
Bubka <858858+Bubka@users.noreply.github.com>
2026-04-03 14:21:28 +0200

    Rebuild assets

---
+++ public/build/assets/bus-DOSnHFcV.js
@@ -0,0 +1,1 @@
+import{ae as e}from"./app-BWmpdjQx.js";const r=e("bus",{state:()=>({migrationUri:null,decodedUri:null,inManagementMode:!1,editedGroupName:null,username:null}),actions:{}});export{r as u};
